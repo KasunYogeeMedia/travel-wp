d2c1e76c72075de162fa9fe05fd9189a6995c08f
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-02-20 15:00:06 +0530

    form 2 styles

--- wp-content/themes/travel/page-form2.php
+++ wp-content/themes/travel/page-form2.php
@@ -60,7 +60,29 @@ get_header();  ?>
                                             <h2 class="mb-4">Choose Hotel</h2>
                                             <div class="row">
                                                 <div class="col-sm-6 col-md-4 col-lg-3 border p-3">
-                                                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/inc/img/home1.jpg" alt="Option 1">
+                                                    <!-- Carosel -->
+                                                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                                        <div class="carousel-inner">
+                                                            <div class="carousel-item active">
+                                                                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/home1.jpg" class="d-block w-100" alt="...">
+                                                            </div>
+                                                            <div class="carousel-item">
+                                                                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/home1.jpg" class="d-block w-100" alt="...">
+                                                            </div>
+                                                            <div class="carousel-item">
+                                                                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/home1.jpg" class="d-block w-100" alt="...">
+                                                            </div>
+                                                        </div>
+                                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                            <span class="visually-hidden">Previous</span>
+                                                        </button>
+                                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                            <span class="visually-hidden">Next</span>
+                                                        </button>
+                                                    </div>
+                                                    <!-- Carosel -->
                                                     <div class="caption row my-2">
                                                         <div class="col">
                                                             <span class="text-primary">Fox Kandy</span>
