0c445f31a02e4e2398fec3bf7bb25d95c448a948
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-03-14 17:55:01 +0530

    login register page tempate

---
+++ wp-content/themes/travel/page-Reg-Log.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Register Login Page
+*/
+get_header('new');  ?>
+
+<!-- ////////////////Register Login Page////////////////// -->
+<div class="log_reg">
+    <?php the_content() ?>
+</div>
+
+<!-- ////////////////Register Login Page////////////////// -->
+
+<?php get_footer('new'); ?>
