6e8604ee66e0d62a0c821a954ac2e3b5195a15ea
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-03-14 18:02:10 +0530

    API

--- wp-content/themes/travel/page-form2.php
+++ wp-content/themes/travel/page-form2.php
@@ -52,6 +52,7 @@ get_header();
                         </div>
                     </div>
                 </div>
+                
                 <div class="container sing_form_sec">
                     <div class="choose_sec py-5 position-relative">
                         <!-- Choose your stay -->
@@ -428,25 +429,6 @@ $theme_url = get_template_directory_uri();
 
                         // Update the selected option data display area
                         updateSelectedOptionBreakfast(imageSrc, title, price, location);
-                        const cookiesObj = {
-                            name: 'Chocolate Chip',
-                            type: 'Soft and Chewy',
-                            quantity: 12
-                        };
-
-                        const cookiesJson = JSON.stringify(cookiesObj);
-
-                        const xhr = new XMLHttpRequest();
-                        xhr.open('POST', '<?php echo $theme_url; ?>/pdf_generator.php');
-                        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                        xhr.onload = () => {
-                            if (xhr.status === 200) {
-                                console.log(xhr.responseText);
-                            } else {
-                                console.error('Error generating PDF');
-                            }
-                        };
-                        xhr.send(`cookiesJson=${encodeURIComponent(cookiesJson)}`);
                     });
                 },
                 error: function(xhr, status, error) {
@@ -783,6 +765,36 @@ $theme_url = get_template_directory_uri();
 
 
 </script>
+<script>
+      // get the form element
+      var form = document.forms[0];
+
+      // get the submit button element
+      var submitBtn = document.getElementById("nextBtn");
+
+      // add a click event listener to the submit button
+      submitBtn.addEventListener("click", function() {
+        // initialize a variable to keep track of the last filled element
+        var lastFilledElement = null;
+
+        // loop through all the form elements
+        for (var i = 0; i < form.elements.length; i++) {
+          var element = form.elements[i];
+
+          // check if the element is a form control that can be filled by the user
+          if (element.type !== "button" && element.type !== "submit" && element.type !== "reset" && !element.disabled) {
+            // check if the element has a value
+            if (element.value !== "") {
+              // update the lastFilledElement variable
+              lastFilledElement = element;
+            }
+          }
+        }
+
+        // display the value of the last filled element in an alert box
+        alert("Last filled element: " + (lastFilledElement ? lastFilledElement.value : "None"));
+      });
+    </script>
 
 <?php get_footer();
 
