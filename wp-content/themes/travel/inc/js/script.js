2ed46727d461db1a5e516c81ac3d468294c6dd06
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-03-14 12:41:52 +0530

    function showTab

--- wp-content/themes/travel/inc/js/script.js
+++ wp-content/themes/travel/inc/js/script.js
@@ -16,15 +16,14 @@ function updateSelectedOptionBreakfast(imageSrc, title, price, location) {
   document.getElementById("btn2_price").innerHTML = price;
   document.getElementById("sel2").value = location;
   var expireDate = new Date();
-  expireDate.setTime(expireDate.getTime() + (60 * 60 * 1000));
+  expireDate.setTime(expireDate.getTime() + 60 * 60 * 1000);
   // Save the data to a cookie
   document.cookie = "imageSrc=" + imageSrc + ";";
   document.cookie = "title=" + title + ";";
   document.cookie = "price=" + price + ";";
   document.cookie = "location=" + location + ";";
-
 }
-// Morning  
+// Morning
 function updateSelectedOptionMorning(imageSrc, title, price, location) {
   document.getElementById("btn3_inside").style.backgroundImage =
     "url('" + imageSrc + "')";
@@ -68,101 +67,120 @@ function updateSelectedOptionNight(imageSrc, title, price, location) {
 
 // Step form scripts
 var currentTab = 0; // Current tab is set to be the first tab (0)
-    showTab(currentTab); // Display the current tab
+showTab(currentTab); // Display the current tab
 
-    function showTab(n) {
-        // This function will display the specified tab of the form...
-        var x = document.getElementsByClassName("tab");
-        x[n].style.display = "block";
-        //... and fix the Previous/Next buttons:
-        if (n == 0) {
-            document.getElementById("prevBtn").style.display = "none";
-        } else {
-            document.getElementById("prevBtn").style.display = "inline";
-        }
-        if (n == (x.length - 1)) {
-            document.getElementById("nextBtnNext").innerHTML = "Submit";
-        } else {
-            document.getElementById("nextBtnNext").innerHTML = "Next <i class='fa fa-arrow-right' aria-hidden='true'></i>";
-        }
-        //... and run a function that will display the correct step indicator:
-        fixStepIndicator(n)
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").style.display = "none";
+    var submitBtn = document.createElement("button");
+    submitBtn.setAttribute("type", "submit");
+    submitBtn.setAttribute("id", "submitBtn");
+    submitBtn.innerHTML = "Submit";
+    document.getElementById("regForm").appendChild(submitBtn);
+  } else {
+    document.getElementById("nextBtn").style.display = "inline";
+    var submitBtn = document.getElementById("submitBtn");
+    if (submitBtn) {
+      submitBtn.parentNode.removeChild(submitBtn);
     }
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
 
-    function nextPrev(n) {
-        // This function will figure out which tab to display
-        var x = document.getElementsByClassName("tab");
-        // Exit the function if any field in the current tab is invalid:
-        // if (n == 1 && !validateForm()) return false;
-        // Hide the current tab:
-        x[currentTab].style.display = "none";
-        // Increase or decrease the current tab by 1:
-        currentTab = currentTab + n;
-        // if you have reached the end of the form...
-        if (currentTab >= x.length) {
-            // ... the form gets submitted:
-             document.getElementById("regForm").submit();
-            let submitButton = document.getElementById("nextBtn");
-              submitButton.addEventListener("click", function(event) {
-                  event.preventDefault(); // Prevent the default form submission behavior
-                  
-                  let formData = {};
-                  let formElements = document.querySelectorAll("#regForm input, #regForm select, #regForm textarea");
 
-                  formElements.forEach(function(element) {
-                      formData[element.name] = element.value;
-                  });
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  // if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    // document.getElementById("regForm").submit();
+    document.getElementById("nextBtn").type = "submit";
 
-                  let jsonData = JSON.stringify(formData);
+    let submitButton = document.getElementById("nextBtn");
+    submitButton.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the default form submission behavior
 
-                  let xhr = new XMLHttpRequest();
-                  xhr.open("POST", "your-url-here", true);
-                  xhr.setRequestHeader("Content-Type", "application/json");
-                  xhr.onreadystatechange = function() {
-                      if (xhr.readyState === 4 && xhr.status === 200) {
-                          console.log(xhr.responseText);
-                      }
-                  };
-                  xhr.send(jsonData);
-              });
-            return false;
+      let formData = {};
+      let formElements = document.querySelectorAll(
+        "#regForm input, #regForm select, #regForm textarea"
+      );
+
+      formElements.forEach(function (element) {
+        formData[element.name] = element.value;
+      });
+
+      let jsonData = JSON.stringify(formData);
+
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "your-url-here", true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          console.log(xhr.responseText);
         }
-        // Otherwise, display the correct tab:
-        showTab(currentTab);
+      };
+      xhr.send(jsonData);
+    });
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x,
+    y,
+    i,
+    valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
     }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
 
-    function validateForm() {
-        // This function deals with validation of the form fields
-        var x, y, i, valid = true;
-        x = document.getElementsByClassName("tab");
-        y = x[currentTab].getElementsByTagName("input");
-        // A loop that checks every input field in the current tab:
-        for (i = 0; i < y.length; i++) {
-            // If a field is empty...
-            if (y[i].value == "") {
-                // add an "invalid" class to the field:
-                y[i].className += " invalid";
-                // and set the current valid status to false
-                valid = false;
-            }
-        }
-        // If the valid status is true, mark the step as finished and valid:
-        if (valid) {
-            document.getElementsByClassName("step")[currentTab].className += " finish";
-        }
-        return valid; // return the valid status
-    }
-
-    function fixStepIndicator(n) {
-        // This function removes the "active" class of all steps...
-        var i, x = document.getElementsByClassName("step");
-        for (i = 0; i < x.length; i++) {
-            x[i].className = x[i].className.replace(" active", "");
-        }
-        //... and adds the "active" class on the current step:
-        x[n].className += " active";
-    }
-
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i,
+    x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
 
 // Add click event listeners to the Choose buttons inside each option
 // document
@@ -186,7 +204,6 @@ var currentTab = 0; // Current tab is set to be the first tab (0)
 // Choose Your Stay scripts
 // Breakfast
 // Function to update the selected option data display area
-
 
 // Navbar toggle
 $(document).ready(function () {
