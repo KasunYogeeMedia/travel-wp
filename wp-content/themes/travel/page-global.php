559089c2253e5e904bfe3717f6d5dd5879765e6f
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-03-15 11:11:02 +0530

    new global page

---
+++ wp-content/themes/travel/page-global.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Global
+*/
+get_header();  ?>
+
+<!-- ////////////////Global page content Start////////////////// -->
+
+<div class="global_content">
+    <?php the_content() ?>
+</div>
+
+<!-- ////////////////Global page content End////////////////// -->
+
+<?php get_footer(); ?>
