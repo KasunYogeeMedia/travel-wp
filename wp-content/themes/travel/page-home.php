fc3a5915defe0a53533f7bd0cfc1627179470a70
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-03-22 15:56:56 +0530

    loop travel posts

--- wp-content/themes/travel/page-home.php
+++ wp-content/themes/travel/page-home.php
@@ -171,7 +171,7 @@ get_header();  ?>
             <div class="container-fluid mt-4">
                 <div class="steps2-images">
                     <div class="row">
-                        <div class="col-sm-6 col-md-6 col-lg-4">
+                        <!-- <div class="col-sm-6 col-md-6 col-lg-4">
                             <div class="card card-action">
                                 <img src="<?php echo get_template_directory_uri(); ?>/inc/img/coronavirus-rome-trapped-in-the-most-beautiful-city-in-the-worldromenew.jpg" class="card-img-top" alt="Perfect Itinerary">
                                 <div class="card-overlay">
@@ -187,93 +187,35 @@ get_header();  ?>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-sm-6 col-md-6 col-lg-4">
-                            <div class="card card-action">
-                                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/HDR-Photography-Beautiful-Paris-Presetpro.com_-1024x683new.jpg" class="card-img-top" alt="Perfect Itinerary">
-                                <div class="card-overlay">
-                                    <div class="card-body">
-                                        <div class="text">
-                                            <a href="blog/">
-                                                <h3 class="countries-name">Paris</h3>
-                                            </a>
-                                            <p class="countries-para"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero distinctio
-                                                tenetur placeat qui pariatur facilis soluta autem quos aliquam velit natus ipsa,
-                                                enim accusantium quae similique, consectetur earum? Minima, explicabo? </p>
+                        </div> -->
+                        <?php
+                        $args = array('post_type' => 'travel_blogs', 'posts_per_page' => 6);
+                        $the_query = new WP_Query($args);
+                        ?>
+                        <?php if ($the_query->have_posts()) : ?>
+                            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                                <div class="col-sm-6 col-md-6 col-lg-4">
+                                    <div class="card card-action">
+                                        <a href="<?php the_permalink(); ?>">
+                                            <img src="<?php the_post_thumbnail_url(); ?>" class="card-img-top" alt="Perfect Itinerary">
+                                        </a>
+                                        <div class="card-overlay">
+                                            <div class="card-body">
+                                                <div class="text">
+                                                    <a href="<?php the_permalink(); ?>">
+                                                        <h3 class="countries-name"><?php the_title(); ?></h3>
+                                                    </a>
+                                                    <p class="countries-para"><?php the_field('hover_description'); ?></p>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-md-6 col-lg-4">
-                            <div class="card card-action">
-                                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/2968902_17101113310057652035dubainew.jpg" class="card-img-top" alt="Perfect Itinerary">
-                                <div class="card-overlay">
-                                    <div class="card-body">
-                                        <div class="text">
-                                            <a href="blog/">
-                                                <h3 class="countries-name">Dubai</h3>
-                                            </a>
-                                            <p class="countries-para"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero distinctio
-                                                tenetur placeat qui pariatur facilis soluta autem quos aliquam velit natus ipsa,
-                                                enim accusantium quae similique, consectetur earum? Minima, explicabo? </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-md-6 col-lg-4">
-                            <div class="card card-action">
-                                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/photo-1555109307-f7d9da25c244newyorknew.jpg" class="card-img-top" alt="Perfect Itinerary">
-                                <div class="card-overlay">
-                                    <div class="card-body">
-                                        <div class="text">
-                                            <a href="blog/">
-                                                <h3 class="countries-name">NYC</h3>
-                                            </a>
-                                            <p class="countries-para"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero distinctio
-                                                tenetur placeat qui pariatur facilis soluta autem quos aliquam velit natus ipsa,
-                                                enim accusantium quae similique, consectetur earum? Minima, explicabo? </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-md-6 col-lg-4">
-                            <div class="card card-action">
-                                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/London-Eye-Pictures-720x404londonnew.jpg" class="card-img-top" alt="Perfect Itinerary">
-                                <div class="card-overlay">
-                                    <div class="card-body">
-                                        <div class="text">
-                                            <a href="blog/">
-                                                <h3 class="countries-name">London</h3>
-                                            </a>
-                                            <p class="countries-para"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero distinctio
-                                                tenetur placeat qui pariatur facilis soluta autem quos aliquam velit natus ipsa,
-                                                enim accusantium quae similique, consectetur earum? Minima, explicabo? </p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-md-6 col-lg-4">
-                            <div class="card card-action">
-                                <img src="<?php echo get_template_directory_uri(); ?>/inc/img/aa43530936bcbc9f6c9bc4d0c2c12474ohunew.jpg" class="card-img-top" alt="Perfect Itinerary">
-                                <div class="card-overlay">
-                                    <div class="card-body">
-                                        <div class="text">
-                                            <a href="blog/">
-                                                <h3 class="countries-name">Oahu</h3>
-                                            </a>
-                                            <p class="countries-para"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero distinctio
-                                                tenetur placeat qui pariatur facilis soluta autem quos aliquam velit natus ipsa,
-                                                enim accusantium quae similique, consectetur earum? Minima, explicabo? </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                            <?php endwhile;
+                            wp_reset_postdata(); ?>
+                        <?php else :  ?>
+                            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
