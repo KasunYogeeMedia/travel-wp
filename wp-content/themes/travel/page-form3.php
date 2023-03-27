63b812149ca04dc8eb9e40de4b1fc2eb227fa81b
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-03-27 13:34:04 +0530

    save pdf

--- wp-content/themes/travel/page-form3.php
+++ wp-content/themes/travel/page-form3.php
@@ -188,7 +188,7 @@ $pdf->Output($upload_path . '/'.$pdf_name.'.pdf', 'F');
 
 session_start();
 $_SESSION["pdf_name"] = $pdf_name;
-do_action('my_custom_action', $_SESSION["pdf_name"]);
+
 ?>
 <div class="global_content">
     <div class="container">
