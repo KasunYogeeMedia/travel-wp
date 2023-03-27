586b55762ed2d853dc937796c34e1b3cfe73b0d6
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-03-27 14:22:13 +0530

    save pdf

--- wp-content/themes/travel/page-form3.php
+++ wp-content/themes/travel/page-form3.php
@@ -43,10 +43,9 @@ $pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8',
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 001');
-$pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+$pdf->SetAuthor('The Perfect Itinerary');
+$pdf->SetTitle('The Perfect Itinerary');
+
 
 // set default header data
 $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE . ' 001', PDF_HEADER_STRING, array(0, 64, 255), array(0, 64, 128));
