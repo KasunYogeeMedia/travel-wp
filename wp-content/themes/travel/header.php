dfa73caa0662b8a191db1b0e290ce7e97c01848b
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-03-27 14:55:51 +0530

    save pdf

--- wp-content/themes/travel/header.php
+++ wp-content/themes/travel/header.php
@@ -22,11 +22,13 @@
 	<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/inc/img/favicon.png">
 
 	<?php wp_head(); ?>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
 	<!-- Bootstrap  -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css" rel="stylesheet" />
 
 	<!-- fontAwsome -->
