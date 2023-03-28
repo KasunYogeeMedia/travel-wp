fce10310a186abfb86933c76618c566ac2869c04
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-03-28 06:43:03 +0530

    register issue

--- wp-content/themes/travel/header.php
+++ wp-content/themes/travel/header.php
@@ -43,7 +43,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css">
 
 	<!-- daterangepicker styles -->
-	<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+	
 	<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js" defer></script>
 	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
