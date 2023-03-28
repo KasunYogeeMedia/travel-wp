a4b77d46bed7dcf43735742baff48c8dcbfee3e8
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-03-28 06:50:35 +0530

    register issue

--- wp-content/themes/travel/header.php
+++ wp-content/themes/travel/header.php
@@ -77,7 +77,20 @@
 						'walker' => new bootstrap_5_wp_nav_menu_walker()
 					));
 					?>
-
+					<div class="collapse navbar-collapse" id="navbar-list-4">
+						<ul class="navbar-nav">
+							<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg" width="40" height="40" class="rounded-circle">
+							</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<a class="dropdown-item" href="#">Dashboard</a>
+							<a class="dropdown-item" href="#">Edit Profile</a>
+							<a class="dropdown-item" href="#">Log Out</a>
+							</div>
+						</li>   
+						</ul>
+					</div>
 					<div class="social-media-icon">
 						<ul class="navbar-nav sm-icons social-icons pt-md-22 px-md-2 d-inline">
 
