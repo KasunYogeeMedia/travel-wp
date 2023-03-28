711cfe74efff352cb268849ef5f825cc2b66e16b
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-03-28 06:21:43 +0530

    register issue

--- wp-content/themes/travel/header-new.php
+++ wp-content/themes/travel/header-new.php
@@ -23,9 +23,7 @@
 
 	
 	<!-- Bootstrap  -->
-	<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script> -->
-
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
 
 
