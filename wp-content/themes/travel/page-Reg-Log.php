686e8c644fcebc6c4a01e1f9419101776b51b3b7
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-03-31 15:55:03 +0530

    login and register pages

--- wp-content/themes/travel/page-Reg-Log.php
+++ wp-content/themes/travel/page-Reg-Log.php
@@ -5,7 +5,7 @@ Template Name: Register Login Page
 get_header('new');  ?>
 
 <!-- ////////////////Register Login Page////////////////// -->
-<div class="log_reg">
+<div class="log_reg" style="background-image: url(<?php echo get_template_directory_uri(); ?>/inc/img/login_back.jpg);">
     <div class="container">
         <?php the_content() ?>
     </div>
